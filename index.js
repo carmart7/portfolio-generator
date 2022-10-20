a3e2b9733c727b16d57474436a1008a96517dcc6
AMESocker <aesocher@yahoo.com>
2022-10-20 11:45:53 -0700

    generater html

--- index.js
+++ index.js
@@ -28,5 +28,27 @@ inquirer.prompt([
         message: 'What is your Github Url?'
     }
 ]).then((data) => {
+    fs.writeFile('portfolio.html',data,callback);
+
     console.log(data);
 })
+function writeHtml(data){
+    let html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio</title>
+    </head>
+    <body>
+        <h1>${data.name}</h1>
+        <ul>
+            <li>Location:${data.location} </li>
+            <li>Bio:${data.bio} </li>
+            <li>LinkedIn Url:${data.linkedinurl} </li>
+            <li>Github Url:${githuburl} </li>
+        </ul>
+    </body>
+    </html>`
+}
